a1fa7e789a4ed19b8623946ffb375592dfb870c1
LuigiBelanda <luigi.belanda.milani@gmail.com>
2021-08-26 18:04:46 -0300

    Add comentarios ao projeto

--- send_sms/process_sms.php
+++ send_sms/process_sms.php
@@ -11,6 +11,7 @@
         return;
     }
     else {
+        // API CALL
         $ch = curl_init('https://textbelt.com/text');
         $data = array(
             'phone' => $phone_number,
@@ -28,6 +29,7 @@
         echo "<br>";
         */
     
+        // GETTING PARTS OF THE API RESPONSE TO SEND A USER RESPONSE
         $result = substr($response, 11, 5);
         $result_quotaRemaining = substr($response, 26, 45);
         $result_number = substr($response, 26, 93);
@@ -44,12 +46,15 @@
     
         curl_close($ch);
 
+        // USER RESPONSE ABOUT YOUR REQUEST
         if ($result === "false") {
             if ($result_number === "Your phone number was not provided in E.164 format, or free SMS are disabled for this country") {
                 echo"<script language='javascript' type='text/javascript'> alert('Número de telefone inválido ou o serviço não está funcionando no seu país!');window.location.href='send_sms.php';</script>";
+                return;
             } 
             if ($result_quotaRemaining === "Only one test text message is allowed per day") {
                 echo"<script language='javascript' type='text/javascript'> alert('Você pode enviar apenas 1 SMS por dia, provavelmente você já mandou um SMS hoje, por isso seu SMS não foi enviado!');window.location.href='send_sms.php';</script>";
+                return;
             }
         }
         else {
